9dd282b133fb23b8a4a8c230feb7b0d51c01b4fa
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-19 23:22:30 -0300

    Feat[projects]add_readme;

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props:ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello React Native</Text>
+      <Button title="confirm"  />
+      <Button title="erase" />
+      <Button title="blank" />
+      <StatusBar style="auto"  backgroundColor='red'/>
     </View>
   );
 }
@@ -13,8 +31,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: 'red',
+    fontSize: 20,
+  },
+  button:{
+    backgroundColor: 'red',
+    borderBottomColor: 'green'
+
+  }
 });
