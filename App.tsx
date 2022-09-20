cc0068f32c26ff4bbcda586524aa66a99bc09583
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-20 17:22:09 -0300

    Feat[app]some_changes;

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
+import { StatusBar } from 'react-native';
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_900Black,
+} from '@expo-google-fonts/inter';
 import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_900Black,
+	});
+
 	return (
 		<Background>
-			<h1>Test</h1>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			{fontsLoaded ? <Home /> : <Loading />}
 		</Background>
 	);
 }
