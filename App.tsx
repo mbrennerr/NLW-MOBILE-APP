d2a2cabd3ff54758c5a0ef58d5bf3aa97bbea64c
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-20 15:56:34 -0300

    Feat[Build]add_src_assets_files;

--- App.tsx
+++ App.tsx
@@ -1,47 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props:ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
+import { Background } from './src/components/Background';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native</Text>
-      <Button title="confirm"  />
-      <Button title="erase" />
-      <Button title="blank" />
-      <StatusBar style="auto"  backgroundColor='red'/>
-    </View>
-  );
+	return (
+		<Background>
+			<h1>Test</h1>
+		</Background>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    color: 'red',
-    fontSize: 20,
-  },
-  button:{
-    backgroundColor: 'red',
-    borderBottomColor: 'green'
-
-  }
-});
