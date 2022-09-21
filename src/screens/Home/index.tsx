61ad0549f96f06012e01997363300abe8c29e88e
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-21 12:00:20 -0300

    Feat[logo]add_logo_to_home_screen;

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
 
 export function Home() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<Image source={logoImg} style={styles.logo} />
+			<Text>
+				Welcome to the NLW Esports app! This is the home screen.
+			</Text>
+		</View>
+	);
 }
