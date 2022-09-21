249faad109e7e6d676b30c1901c5dd924fabed6b
mbrennerr <marciobrennerbusiness@gmail.com>
2022-09-21 18:24:12 -0300

    Feat[screens]add_component_heading_to_home_screen;

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, Text, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
@@ -12,6 +13,11 @@ export function Home() {
 			<Text>
 				Welcome to the NLW Esports app! This is the home screen.
 			</Text>
+
+			<Heading
+				title='Encontre seu duo!'
+				subtitle='Selecione o game que deseja jogar'
+			/>
 		</View>
 	);
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-	container: { flex: 1, alignItems: 'center' },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+	},
 	logo: {
 		width: 214,
 		height: 120,
